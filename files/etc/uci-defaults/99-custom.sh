9751e08cbb63e77c122495b433698ec9522e3dd4
chinaboy2345 <54342248+chinaboy2345@users.noreply.github.com>
2025-03-29 09:05:17 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,11 +1,3 @@
-# 设置默认wan口防火墙打开 方便虚拟机用户首次访问webui
-uci set firewall.@zone[1].input='ACCEPT'
-uci commit firewall
-# 设置主机名映射 解决安卓原生TV首次连不上网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
-uci commit dhcp
 # 根据网卡数量配置网络
 count=0
 for iface in $(ls /sys/class/net | grep -v lo); do
